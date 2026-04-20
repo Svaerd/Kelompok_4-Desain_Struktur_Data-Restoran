a518fdf3569b15dbefefa5f6fc9c1a2812e7e13d
4host <husam.danish@proton.me>
2026-04-20 12:07:34 +0700

    call printOrderItems func in Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    // Nama: Husam Danish
+    // NRP: 5027251060
+    System.out.println("\n--- LAPORAN SISTEM ---");
+    LinkedListService.printOrderItems("ORD-001");
+  }
+}
