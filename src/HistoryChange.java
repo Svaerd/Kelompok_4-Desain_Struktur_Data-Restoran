bd1ba177e19860fed63dc880b7580ad051bb365e
Satyzfying <satyaputraaryanta@gmail.com>
2026-04-20 11:35:27 +0700

    add HistoryChange

---
+++ src/HistoryChange.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class HistoryChange {
+    String status;
+    String timestamp;
+
+    public HistoryChange(String status, String timestamp) {
+        this.status = status;
+        this.timestamp = timestamp;
+    }
+}
